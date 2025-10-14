6e17d96665b8f3960f1b324ad316b774379dbecb
Hector <hectorsoria2@outlook.com>
2025-10-14 16:33:20 -0300

    Feat(pagamento):Melhoria no codigo para melhor visualizadao

--- sistema-flow/src/routes/Pagamento/index.tsx
+++ sistema-flow/src/routes/Pagamento/index.tsx
@@ -8,10 +8,26 @@ export default function Pagamento(){
     document.title = "Pagamento";
     return(
         <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Nome no cartão" />
-                <input type="text" placeholder="Número do cartão" />
-                <input type="text" placeholder="CVV" />
+           <form onSubmit={handleSubmit}>
+                <h1 >Pagamento</h1>
+                <input
+                    type="text"
+                     placeholder="Nome no cartão"
+                 />
+                 <input
+                    type="text"
+                    placeholder="Número do cartão"
+                />
+                <input
+                    type="text"
+                    placeholder="Data de validade"
+                />
+                <input
+                    type="text"
+                    placeholder="CVV"
+                />
+                <input type="text"
+                placeholder="CPF do titular" />
                 <button type="submit">Pagar</button>
             </form>
         </div>
