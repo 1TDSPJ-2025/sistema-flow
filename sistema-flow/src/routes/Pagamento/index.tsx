1ec1d75b09665464b5862e32f3c9da415adf7a9f
Hector <hectorsoria2@outlook.com>
2025-10-14 10:02:37 -0300

    Feat(pagamento): Criacao da rota para pagamento e imputs para o cartao

--- sistema-flow/src/routes/Pagamento/index.tsx
+++ sistema-flow/src/routes/Pagamento/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
+
 export default function Pagamento(){
     document.title = "Pagamento";
     return(
-        <main>
-            <h1>Pagina de Pagamento</h1>
-        </main>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Nome no cartão" />
+                <input type="text" placeholder="Número do cartão" />
+                <input type="text" placeholder="CVV" />
+                <button type="submit">Pagar</button>
+            </form>
+        </div>
     );
 
 
