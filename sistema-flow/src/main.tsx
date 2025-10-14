118a02cede777ec4b956a7fde804568e9706ff54
JoaoGabriel76 <joaogabrielf840@gmail.com>
2025-10-14 10:06:48 -0300

    Instalação Tailwind

--- sistema-flow/src/main.tsx
+++ sistema-flow/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import './styles.css'
+
 
 import App from './App.tsx'
 import Error from './routes/Error/index.tsx';
