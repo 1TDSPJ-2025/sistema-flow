7f219d3ab4a4a1a79bdec2b6d3bf2dca37599e68
manu <haru.noom0@gmail.com>
2025-10-14 08:45:24 -0300

    começando a rota criar conta e adicionando a api ,register e handleSubmit

--- sistema-flow/src/routes/CriarConta/index.tsx
+++ sistema-flow/src/routes/CriarConta/index.tsx
@@ -1,12 +1,46 @@
 export default function CriarConta(){
-    document.title = "Criar Conta";
 
-    return(
-        <main>
+    type CadastroData = {
+    nome: string;
+    nomeUsuario: string;
+    email: string;
+    };
 
-            <h1>Criar Conta</h1>
+    export default function Cadastro() {
+      const { register, handleSubmit, formState: { errors } } = useForm<CadastroData>();
+      const navigate = useNavigate();
 
+      const onSubmit = async (data: CadastroData) => {
+        const res = await fetch(`http://localhost:3000/usuarios?nomeUsuario=${data.nomeUsuario}&email=${data.email}`);
+
+        const users: Usuario[] = await res.json();
+        const usuarioExistente = users.find(
+
+
+          (u) => u.nomeUsuario === data.nomeUsuario || u.email === data.email
+        );
+
+        if (usuarioExistente) {
+
+          alert("Nome de usuário ou email já cadastrado!");
+          return;
+        }
+
+        await fetch("http://localhost:3000/usuarios", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data)
+
+          
+        });
+
+        alert("Cadastro realizado com sucesso!");
+        navigate("/login");
+      };
+
+        return(
             
-        </main>
-    );
+        );
+    }
+
 }
