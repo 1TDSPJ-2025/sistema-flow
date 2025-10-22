7e0d27f45982a883523c9b5cfe355ac434eac21c
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-10-22 11:26:49 -0300

    fix:Apagando pastas desnecessárias

--- sistema-flow/src/routes/Produtos/index.tsx
+++
@@ -1,9 +0,0 @@
-//import Card from "../../components/Card/Card";
-//import Title from "../../components/H1Title/Title";
-//import infos from "../../api/produtos.json"
-
-export default function Produtos(){
-    document.title = "Produtos";
-
-    
-}
